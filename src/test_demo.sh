3983772d875f213187948e65143737ba9edb0d5f
Amir Goldstein <amir73il@gmail.com>
2019-12-23 19:50:08 +0200

    test_demo script
    Run inotifywatch with 2 sec linger, generate some dir modify events
    then process and report.
    
    When running as user root use global watch, otherwise use
    unprivileged recursive watches.
    
    Signed-off-by: Amir Goldstein <amir73il@gmail.com>

---
+++ src/test_demo.sh
@@ -0,0 +1,45 @@
+#!/bin/sh -x
+
+WD=${1:-"."}
+cd $WD
+
+#echo "file fs/notify/fanotify/*  +p" > /sys/kernel/debug/dynamic_debug/control
+rm -rf a
+mkdir -p a/b/c/d/e/f/g/
+touch a/b/c/0 a/b/c/1 a/b/c/d/e/f/g/0
+
+if [ $(id -u) = 0 ]; then
+	# FAN_MARK_FILESYSTEM and open_by_handle_at(2)
+	MODE='--global'
+else
+	# FAN_UNPRIVILEGED recursive watches
+	MODE='--recursive'
+fi
+
+# Sleep 2 seconds while generating events and then process events
+EVENTS="-e dir_modify"
+inotifywatch $MODE $EVENTS --timeout -2 $WD &
+
+sleep 1
+#SLEEP='sleep 1'
+$SLEEP
+t="Create files and dirs..."
+touch a/0 a/1 a/2 a/3
+mkdir a/dir0 a/dir1 a/dir2
+$SLEEP
+t="Rename files and dirs..."
+mv a/0 a/3
+mv a/dir0 a/dir3
+$SLEEP
+t="Delete files and dirs..."
+rm a/1
+rmdir a/dir1
+$SLEEP
+t="Modify files and dirs..."
+chmod +x a/b/c/d
+echo >> a/b/c/0
+$SLEEP
+t="Move files and dirs..."
+mv a/b/c/1 a/b/c/d/e/f/g/1
+mv a/b/c/d/e/f/g a/b/c/d/e/G
+$SLEEP
